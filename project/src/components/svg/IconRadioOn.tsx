eb4b818a8511906f68f23c7ab1072ac38844d5e1
Franko Zarkovic <septoforcemc@gmail.com>
2024-05-30 03:07:37 +0200

    Added more Icon components

---
+++ project/src/components/svg/IconRadioOn.tsx
@@ -0,0 +1,13 @@
+export function IconRadioOn(props: { color?: string; size?: string }) {
+  const size = props.size ?? '24'
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M12 7c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zm0-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"
+        fill={props.color || 'var(--surface-surface-1)'}
+      />
+    </svg>
+  )
+}
+
+export default IconRadioOn
